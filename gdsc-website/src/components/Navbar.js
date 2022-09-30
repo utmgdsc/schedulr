1624d266bfa846f0e21b6e65ee137089babc9ff5
Nicholas Ospina <55293276+NicholasOspina@users.noreply.github.com>
2022-09-30 15:27:20 -0400

    First Commit

---
+++ gdsc-website/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import './Navbar.css';
+
+
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className='navbar-container'>
+                    Home
+                </div>    
+            </nav>
+        </>
+    )
+        
+    
+}
+export default Navbar
