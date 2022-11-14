83b294301827104e32429a3e0d78e55f39c64cbb
Nicholas Ospina <nicholas.ospina03@gmail.com>
2022-11-13 21:27:45 -0500

    home page basic layout finished

--- gdsc_demo_1/src/Home.js
+++ gdsc_demo_1/src/Home.js
@@ -1,8 +1,54 @@
 import React from 'react'
+import './Home.css'
+import NavBar from './components/nav-bar.js'
+import Footer from './components/footer.js'
+import Calendar from './components/calendar.js'
+// import Button from 'react'
 
-function Home() {
+
+function Home() { // import the react calendar api, put in dummy data, keep track of what data needs to go in the calendar
+  
+  //material ui library
+  // any ui's i install make a list
   return (
-    <div>Home</div>
+    
+    <div className='Home'>
+      
+        <NavBar></NavBar>
+        
+        <div className='main-section'>
+          <div className='header'>
+            Last Name, First Name
+
+
+
+          </div>
+          
+          <div className='calendar'>
+
+
+          </div>
+
+          
+          <Footer>
+
+          </Footer>
+        </div>
+        
+      
+
+
+
+
+      <div></div>
+
+
+    </div>
+    
+    
+    
+
+
   )
 }
 
